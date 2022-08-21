d0bcc06e0a0cc5cd7aaa3fe72506c47ef3cf0ac5
vermaritika0509 <97594978+vermaritika0509@users.noreply.github.com>
2022-08-21 13:48:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+noseX = 0;
+noseY= 0;
+
+function preload() 
+{
+  clown_nose = loadImage('https://www.pngitem.com/middle/JJTmhT_clown-nose-png-clip-art-clown-nose-transparent/');
+}
+
+function setup()
+{
+    canvas = createCanvas(300 , 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotposes);
+
+}
+
+function modelLoaded()
+{
+    console.log('poseNet is Initialized');
+}
+
+function draw()
+{
+  image(video , 0 , 0 , 300 , 300);
+  fill(255,0,0);
+  stroke(255,0,0);
+  circle(noseX , noseY , 20);
+  image(clown_nose  , noseX , noseY , 30 , 30 );
+
+}
+
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
+
+function gotposes(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
+
+    }
+}
